91fb0fa11c72fd962ed2673df32cd508ee5fa62a
Wu Yongwei <65208655+Wu-yongwei@users.noreply.github.com>
2020-05-21 19:33:53 +0800

    22222222

--- include/monster.h
+++ include/monster.h
@@ -5,19 +5,16 @@ class monster: public object
 {
 private:
     int healthPoint, defence, attack;
-    point position;
-    int id;
+    int money;
 public:
     virtual void takeEvent(point destination, theBrave &aStrongBrave, tools &usefulTools, map &theBigMap);
-    monster();
+    monster(){};
     ~monster() {};
-    point getPosition() {return position;}
     int getHealthPoint() {return healthPoint;}
     int getDefence() {return defence;}
     int getAttack() {return attack;}
-    int getId() {return id;}
-    void setId(int id)  {this->id = id;}
-    void setPosition(point position) {this->position = position;}
+    int getMoney() {return money;}
+    void setMoney(int money) {this->money = money;}
     void setHealthPoint(int healthPoint) {this->healthPoint = healthPoint;}
     void setDefence(int defence) {this->defence = defence;}
     void setAttack(int attack) {this->attack = attack;}
