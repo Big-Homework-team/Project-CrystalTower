7acaacce635d9c526c5abea077a88e75d1c89c0c
CenariusYZ <409004007@qq.com>
2020-05-21 20:04:19 +0800

    新建侧栏属性栏，属性贴图，背景

--- include/runGame.h
+++ include/runGame.h
@@ -2,11 +2,13 @@
 #include "theBrave.h"
 #include "tools.h"
 #include "map.h"
+#include "picture.h"
 
 class runGame
 {
 private:
-    void initialize(theBrave &aStrongBrave, tools &usefulTools, map &theBigMap); //图形界面初始化
+    void initialize(theBrave &aStrongBrave, tools &usefulTools, map &theBigMap, picture &pic); //图形界面初始化
+    picture pic;
 public:
     void run(); //开始游戏
 };
