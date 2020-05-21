6b20b09d132f955899d81e30873198e60726b0b4
Ericsii <30063838+Ericsii@users.noreply.github.com>
2020-05-21 17:47:33 +0800

    Revert "添加生物基类"
    This reverts commit c57c4cb85da75c6cb29f142ff3f6469c34c1d5a6.

--- include/object.h
+++ include/object.h
@@ -3,28 +3,9 @@
 #include "theBrave.h"
 #include "tools.h"
 #include "map.h"
-#include <string>
 
 class object
 {
 public:
     virtual void takeEvent(point destination, theBrave &aStrongBrave, tools &usefulTools, map &theBigMap) = 0;
-};
-
-class MCreature: public object {
-private:
-    int healthPoint, defence, attack;
-    std::string name;
-    point position;
-public:
-    MCreature();
-    MCreature(std::string name_, int healthPoint_, int defence, int attack);
-
-    point getPosition() { return this->position; }
-    void setPosition(point position_) { this->position = position_; }
-    void setName(std::string name_);
-
-    virtual void takeAttack(MCreature * obj) = 0;
-    virtual void takeHurt(MCreature * from) = 0;
-    virtual void takeAttackFightBack(MCreature * obj) = 0;
 };
