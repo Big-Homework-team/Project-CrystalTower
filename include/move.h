d4b70ee473b3149e3d11075d3d2a1b14418ca404
saduj39 <1057476025@qq.com>
2020-05-21 21:49:28 +0800

    修复了商店bug，移除了不需要的move类

--- include/move.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "theBrave.h"
-#include "map.h"
-
-void move(char action, theBrave &aStrongBrave, map &theBigMap); //处理移动
--- src/move.cpp
+++
@@ -1,9 +0,0 @@
-#include "move.h"
-
-#include <iostream>
-using namespace std;
-void move(char action, theBrave &aStrongBrave, map &theBigMap) //处理移动
-{
-    //考虑移动是否合法，如果合法就移动，并修改对象的信息
-    cout << "now_move" << endl;
-}
