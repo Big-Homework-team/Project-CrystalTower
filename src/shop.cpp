bc3096d4a6d1e867abd3fe2072ec81f0eea19839
saduj39 <1057476025@qq.com>
2020-05-21 22:03:41 +0800

    修改商店金钱bug

--- src/shop.cpp
+++ src/shop.cpp
@@ -4,12 +4,11 @@
 void shop::takeEvent(point destination, theBrave &aStrongBrave, tools &usefulTools, map &theBigMap)
 {
 
-    setfillcolor(BLACK);                   //设置背景填充色为黑
+    setfillcolor(DARKGRAY);                //设置背景填充色为深灰
     bar(5 * 32, 9 * 32, 18 * 32, 14 * 32); //绘制矩形作为背景
-
-    setcolor(WHITE);        //字体颜色
-    setfont(16, 0, "宋体"); //文字高度16像素，宽度自适应，字体类型
-    setbkmode(TRANSPARENT); //字体背景色块调为透明
+    setcolor(WHITE);                       //字体颜色
+    setfont(16, 0, "宋体");                //文字高度16像素，宽度自适应，字体类型
+    setbkmode(TRANSPARENT);                //字体背景色块调为透明
     outtextxy(5 * 32, 9 * 32, "商会老板 罗薇娜：");
     outtextxy(5 * 32, 9 * 32 + 1 * 16, "亲爱的光之战士,");
     outtextxy(5 * 32, 9 * 32 + 2 * 16, "您可以通过花费20金币来增强自己。");
@@ -20,7 +19,7 @@ void shop::takeEvent(point destination, theBrave &aStrongBrave, tools &usefulToo
     }
     if (action == 13)
     {
-        setfillcolor(BLACK);
+        setfillcolor(DARKGRAY);
         bar(5 * 32, 9 * 32, 18 * 32, 14 * 32);
         outtextxy(5 * 32, 9 * 32, "商会老板 罗薇娜：");
         outtextxy(5 * 32, 9 * 32 + 2 * 16, "请输出数字代表选择：");
@@ -32,28 +31,47 @@ void shop::takeEvent(point destination, theBrave &aStrongBrave, tools &usefulToo
         {
             action = getch();
         }
-        if (action == '1' || action == '2' || action == '3' || action == '4')
+        if (action == '1' || action == '2' || action == '3')
         {
-            if (action == '1')
+            if (usefulTools.getMoney() >= 20)
             {
-                aStrongBrave.setHealthPoint(aStrongBrave.getHealthPoint() + 400);
-                usefulTools.setMoney(usefulTools.getMoney() - 20);
+                if (action == '1')
+                {
+                    aStrongBrave.setHealthPoint(aStrongBrave.getHealthPoint() + 400);
+                    usefulTools.setMoney(usefulTools.getMoney() - 20);
+                }
+                if (action == '2')
+                {
+                    aStrongBrave.setAttack(aStrongBrave.getAttack() + 3);
+                    usefulTools.setMoney(usefulTools.getMoney() - 20);
+                }
+                if (action == '3')
+                {
+                    aStrongBrave.setDefence(aStrongBrave.getDefence() + 4);
+                    usefulTools.setMoney(usefulTools.getMoney() - 20);
+                }
+                setfillcolor(DARKGRAY);
+                bar(5 * 32, 9 * 32, 18 * 32, 14 * 32);
+                outtextxy(5 * 32, 9 * 32, "商会老板 罗薇娜：");
+                outtextxy(5 * 32, 9 * 32 + 2 * 16, "感谢您的惠顾，");
+                outtextxy(5 * 32, 9 * 32 + 3 * 16, "愿十二神保佑您。");
             }
-            if (action == '2')
+            else
             {
-                aStrongBrave.setAttack(aStrongBrave.getAttack() + 3);
-                usefulTools.setMoney(usefulTools.getMoney() - 20);
+                setfillcolor(DARKGRAY);
+                bar(5 * 32, 9 * 32, 18 * 32, 14 * 32);
+                outtextxy(5 * 32, 9 * 32, "商会老板 罗薇娜：");
+                outtextxy(5 * 32, 9 * 32 + 2 * 16, "您的钱还不够进行购买，");
+                outtextxy(5 * 32, 9 * 32 + 3 * 16, "可以考虑通过讨伐魔物赚取金币。");
             }
-            if (action == '3')
-            {
-                aStrongBrave.setDefence(aStrongBrave.getDefence() + 4);
-                usefulTools.setMoney(usefulTools.getMoney() - 20);
-            }
-            setfillcolor(BLACK);
+        }
+        else
+        {
+            setfillcolor(DARKGRAY);
             bar(5 * 32, 9 * 32, 18 * 32, 14 * 32);
             outtextxy(5 * 32, 9 * 32, "商会老板 罗薇娜：");
-            outtextxy(5 * 32, 9 * 32 + 2 * 16, "感谢您的惠顾，");
-            outtextxy(5 * 32, 9 * 32 + 3 * 16, "愿十二神保佑您。");
+            outtextxy(5 * 32, 9 * 32 + 2 * 16, "考虑后再进行购买是明智的行为，");
+            outtextxy(5 * 32, 9 * 32 + 3 * 16, "期待您的下次光临。");
         }
     }
 }
