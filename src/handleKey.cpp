eadb793da7e6a8c2e628fb21b4be7f34c3f597b9
Mizuhara-yaguchi <380665279@qq.com>
2020-05-21 10:16:18 +0800

    支持上下左右键移动

--- src/handleKey.cpp
+++ src/handleKey.cpp
@@ -9,10 +9,10 @@ void handle(char action, theBrave &aStrongBrave, tools &usefulTools, map &theBig
     action = tolower(action); //转换大小写
     point position, destination;
     destination = position = aStrongBrave.getPosition();
-    if(action == 'a') destination.setX(position.getX() - 1);
-    if(action == 'd') destination.setX(position.getX() + 1);
-    if(action == 'w') destination.setY(position.getY() - 1);
-    if(action == 's') destination.setY(position.getY() + 1);
+    if(action == 'a' || action == 37) destination.setX(position.getX() - 1);
+    if(action == 'd' || action == 39) destination.setX(position.getX() + 1);
+    if(action == 'w' || action == 38) destination.setY(position.getY() - 1);
+    if(action == 's' || action == 40) destination.setY(position.getY() + 1);
     if(theBigMap.getPoint(destination) == 0)
     {
         aStrongBrave.setPosition(destination);
