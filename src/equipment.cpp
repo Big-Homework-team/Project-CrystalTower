9f1264879bdda5c2ea8c83a84d9fc4b7cc150a7c
Mizuhara-yaguchi <380665279@qq.com>
2020-05-21 13:46:38 +0800

    实现了加属性的道具

--- src/equipment.cpp
+++ src/equipment.cpp
@@ -2,5 +2,18 @@
 
 void equipment::takeEvent(point destination, theBrave &aStrongBrave, tools &usefulTools, map &theBigMap)
 {
-    return ;
+    int id = theBigMap.getPoint(destination);
+    if(id == 19)
+    {
+        aStrongBrave.setAttack(aStrongBrave.getAttack() + 10);
+        usefulTools.setWeapon(1);
+        theBigMap.setPoint(destination, 0);
+    }
+    else if(id == 20)
+    {
+        aStrongBrave.setDefence(aStrongBrave.getDefence() + 10);
+        usefulTools.setArmor(1);
+        theBigMap.setPoint(destination, 0);
+    }
+    if(id == 19 || id == 20) aStrongBrave.setPosition(destination);
 }
--- src/prop.cpp
+++ src/prop.cpp
@@ -2,5 +2,26 @@
 
 void prop::takeEvent(point destination, theBrave &aStrongBrave, tools &usefulTools, map &theBigMap)
 {
-    return ;
+    int id = theBigMap.getPoint(destination);
+    if(id == 15)
+    {
+        aStrongBrave.setAttack(aStrongBrave.getAttack() + 1);
+        theBigMap.setPoint(destination, 0);
+    }
+    else if(id == 16)
+    {
+        aStrongBrave.setDefence(aStrongBrave.getDefence() + 1);
+        theBigMap.setPoint(destination, 0);
+    }
+    else if(id == 17)
+    {
+        aStrongBrave.setHealthPoint(aStrongBrave.getHealthPoint() + 100);
+        theBigMap.setPoint(destination, 0);
+    }
+    else if(id == 18)
+    {
+        aStrongBrave.setHealthPoint(aStrongBrave.getHealthPoint() + 200);
+        theBigMap.setPoint(destination, 0);
+    }
+    if(id == 15 || id == 16 || id == 17 || id == 18) aStrongBrave.setPosition(destination);
 }
