dbc60f38e61d12f091315d73aed29039a1b89447
saduj39 <1057476025@qq.com>
2020-05-24 00:31:41 +0800

    增加隐藏道具提醒，修改台词

--- src/shop.cpp
+++ src/shop.cpp
@@ -10,10 +10,10 @@ void shop::takeEvent(point destination, theBrave &aStrongBrave, tools &usefulToo
     setcolor(WHITE);                       //字体颜色
     setfont(16, 0, "黑体");                //文字高度16像素，宽度自适应，字体类型
     setbkmode(TRANSPARENT);                //字体背景色块调为透明
-    outtextxy(5 * 32, 9 * 32, "商会老板 罗薇娜：");
-    outtextxy(5 * 32, 9 * 32 + 2 * 16, "亲爱的光之战士,");
-    outtextxy(5 * 32, 9 * 32 + 3 * 16, "您可以通过花费金币来增强自己。");
-    outtextxy(5 * 32, 9 * 32 + 4 * 16, "第一次需花费20金币，之后每次购买的花费为上次的两倍。");
+    outtextxy(5 * 32, 9 * 32 + 0 * 16, "商会老板 罗薇娜：");
+    outtextxy(5 * 32, 9 * 32 + 1 * 16, "亲爱的光之战士,");
+    outtextxy(5 * 32, 9 * 32 + 2 * 16, "您可以通过花费金币来增强自己。");
+    outtextxy(5 * 32, 9 * 32 + 3 * 16, "第一次需花费20金币，之后每次购买的花费为上次的两倍。");
     char action = '.';
     int cost = pow(2, aStrongBrave.getTimes()) * 10;
     while (action != 32)
@@ -64,24 +64,24 @@ void shop::takeEvent(point destination, theBrave &aStrongBrave, tools &usefulToo
                     usefulTools.setMoney(usefulTools.getMoney() - cost);
                 }
                 bar(5 * 32, 9 * 32, 18 * 32, 14 * 32);
-                outtextxy(5 * 32, 9 * 32, "商会老板 罗薇娜：");
-                outtextxy(5 * 32, 9 * 32 + 2 * 16, "感谢您的惠顾，");
-                outtextxy(5 * 32, 9 * 32 + 3 * 16, "愿十二神保佑您。");
+                outtextxy(5 * 32, 9 * 32 + 0 * 16, "商会老板 罗薇娜：");
+                outtextxy(5 * 32, 9 * 32 + 1 * 16, "感谢您的惠顾，");
+                outtextxy(5 * 32, 9 * 32 + 2 * 16, "愿十二神保佑您。");
             }
             else
             {
                 bar(5 * 32, 9 * 32, 18 * 32, 14 * 32);
-                outtextxy(5 * 32, 9 * 32, "商会老板 罗薇娜：");
-                outtextxy(5 * 32, 9 * 32 + 2 * 16, "您的钱还不够进行购买，");
-                outtextxy(5 * 32, 9 * 32 + 3 * 16, "可以考虑通过讨伐魔物赚取金币。");
+                outtextxy(5 * 32, 9 * 32 + 0 * 16, "商会老板 罗薇娜：");
+                outtextxy(5 * 32, 9 * 32 + 1 * 16, "您的钱还不够进行购买，");
+                outtextxy(5 * 32, 9 * 32 + 2 * 16, "可以考虑通过讨伐怪物赚取金币。");
             }
         }
         else if (choice == 3)
         {
             bar(5 * 32, 9 * 32, 18 * 32, 14 * 32);
-            outtextxy(5 * 32, 9 * 32, "商会老板 罗薇娜：");
-            outtextxy(5 * 32, 9 * 32 + 2 * 16, "考虑后再进行购买是明智的行为，");
-            outtextxy(5 * 32, 9 * 32 + 3 * 16, "期待您的下次光临。");
+            outtextxy(5 * 32, 9 * 32 + 0 * 16, "商会老板 罗薇娜：");
+            outtextxy(5 * 32, 9 * 32 + 1 * 16, "考虑后再进行购买是明智的行为，");
+            outtextxy(5 * 32, 9 * 32 + 2 * 16, "期待您的下次光临。");
         }
         action = '.';
         while (action != 32)
